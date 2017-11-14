23e6905d514f142135b9d84c541b240e0c712b2e
Nick Kronenberg <nikro412956@gmail.com>
2017-11-14 10:55:47 +0100

    no message

---
+++ src/main/java/Travis2/tostie.java
@@ -0,0 +1,9 @@
+package Travis2;
+
+public class tostie {
+    private static int i = 9;
+
+    public int getI(){
+        return i;
+    }
+}
---
+++ src/test/java/Travis2/tostieijzer.java
@@ -0,0 +1,21 @@
+package Travis2;
+
+import Travis.test;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class tostieijzer {
+    private tostie t;
+
+    @Before
+    public void setUp(){
+        t = new tostie();
+    }
+
+    @Test
+    public void testt(){
+        assertEquals(6, t.getI());
+    }
+}
